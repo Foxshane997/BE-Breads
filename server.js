5b776ef06b6290259c25b2246f56ff818ca1f869
“Foxshane997” <Foxshane997@gmail.com>
2024-05-06 18:59:13 -0700

    sending index and show data

--- server.js
+++ server.js
@@ -10,14 +10,20 @@ const app = express()
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
-
+app.use(express.static('public'))
 
 // Routes
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads')
   })
+
+  // 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
+})
+
   
-  // Breads
+// Breads
   const breadsController = require('./controllers/breads_controller.js')
   app.use('/breads', breadsController)
   
