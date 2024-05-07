84cb5d2179298d8fbb0f0d1feecc049715926004
“Foxshane997” <Foxshane997@gmail.com>
2024-05-06 21:16:15 -0700

    create and new routes complete

--- views/index.jsx
+++ views/index.jsx
@@ -1,5 +1,5 @@
 const React = require('react')
-const Default = require('./layouts/default.jsx')
+const Default = require('./layouts/Default.jsx')
 const breads = require('../controllers/breads_controller.js')
 const { title } = require('process')
 
@@ -8,6 +8,9 @@ function Index ({breads, title})  {
         <Default title = {title}>
         <h2>Index Page</h2>
         <ul>
+        <div className="newButton">
+            <a href="/breads/new"><button>Add a new bread</button></a>
+        </div>
         {
             breads.map((bread, index)=> {
                 return (
