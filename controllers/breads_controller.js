556203732eeb9bdab0efea392b66af1176335477
“Foxshane997” <Foxshane997@gmail.com>
2024-05-08 19:55:11 -0700

    added a delete function

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -22,12 +22,14 @@ breads.get('/new', (req, res) => {
 breads.get('/:arrayIndex', (req, res) => {
   if (Bread[req.params.arrayIndex]) {
     res.render('Show', {
-      bread:Bread[req.params.arrayIndex]
+      bread:Bread[req.params.arrayIndex],
+      index: req.params.arrayIndex,
     })
   } else {
-    res.send('error404')
+    res.render('404')
   }
 })
+
   
 // CREATE
 breads.post('/', (req, res) => {
@@ -42,5 +44,12 @@ breads.post('/', (req, res) => {
   Bread.push(req.body)
   res.redirect('/breads')
 })
+
+// DELETE
+breads.delete('/:indexArray', (req, res) => {
+  Bread.splice(req.params.indexArray, 1)
+  res.status(303).redirect('/breads')
+})
+
 
 module.exports = breads
