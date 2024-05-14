9c71094d65845f594e42d86f5bb53c52f8226857
“Foxshane997” <Foxshane997@gmail.com>
2024-05-13 20:38:29 -0700

    Added helper methods for index, create, & show routes

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -5,12 +5,13 @@ const { title } = require('process')
 
 // INDEX
 breads.get('/', (req, res) => {
-  res.render('Index',
-    {
-      breads: Bread,
-      title: 'Index Page'
-    }
-  )
+  Bread.find()
+      .then(foundBreads => {
+          res.render('index', {
+              breads: foundBreads,
+              title: 'Index Page'
+          })
+      })
 })
 
 // NEW Route
@@ -19,29 +20,29 @@ breads.get('/new', (req, res) => {
 })
 
 // SHOW
-breads.get('/:arrayIndex', (req, res) => {
-  if (Bread[req.params.arrayIndex]) {
-    res.render('Show', {
-      bread:Bread[req.params.arrayIndex],
-      index: req.params.arrayIndex,
+breads.get('/:id', (req, res) => {
+  Bread.findById(req.params.id)
+    .then(foundBread => {
+      res.render('show', {
+        bread: foundBread
+      })
     })
-  } else {
-    res.render('404')
-  }
+    .catch(err => {
+      res.send('404')
+    })
 })
-
   
 // CREATE
 breads.post('/', (req, res) => {
-  if (!req.body.image) {
-    req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+  if(!req.body.image) {
+      req.body.image = undefined 
   }
   if(req.body.hasGluten === 'on') {
     req.body.hasGluten = true
   } else {
     req.body.hasGluten = false
   }
-  Bread.push(req.body)
+  Bread.create(req.body)
   res.redirect('/breads')
 })
 
@@ -53,3 +54,6 @@ breads.delete('/:indexArray', (req, res) => {
 
 
 module.exports = breads
+
+// Put this down here incase I need it again
+// req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
