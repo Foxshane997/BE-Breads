f5113c1133b475d60633338820b1efe36f63142f
“Foxshane997” <Foxshane997@gmail.com>
2024-05-13 19:22:43 -0700

    Added schema to bread.js

--- models/bread.js
+++ models/bread.js
@@ -1,23 +1,41 @@
-module.exports = [
-    {
-      name: 'Rye',
-      hasGluten: true,
-      image: 'https://images.unsplash.com/photo-1595535873420-a599195b3f4a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
-    },
-    {
-      name: 'French',
-      hasGluten: true,
-       image: 'https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
-    },
-    {
-      name: 'Gluten-Free',
-      hasGluten: false,
-      image: 'https://images.unsplash.com/photo-1546538490-0fe0a8eba4e6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
-    },
-    {
-      name: 'Pumpernickel',
-      hasGluten: true,
-      image: 'https://images.unsplash.com/photo-1586444248902-2f64eddc13df?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
-    }
-  ]
+// Require Mongoose
+const mongoose = require('mongoose')
+// Shorthand for the constructor
+const { Schema } = mongoose 
+
+// Constructor
+const breadSchema = new Schema ({
+    name: { type: String, required: true},
+    hasGluten: Boolean,
+    Image: { type: String, default: 'https://images.pexels.com/photos/745988/pexels-photo-745988.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'}
+})
+
+// Model
+const Bread = mongoose.model('Bread', breadSchema)
+
+// Exporting Bread
+module.exports = Bread
+
+// module.exports = [
+//     {
+//       name: 'Rye',
+//       hasGluten: true,
+//       image: 'https://images.unsplash.com/photo-1595535873420-a599195b3f4a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+//     },
+//     {
+//       name: 'French',
+//       hasGluten: true,
+//        image: 'https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+//     },
+//     {
+//       name: 'Gluten-Free',
+//       hasGluten: false,
+//       image: 'https://images.unsplash.com/photo-1546538490-0fe0a8eba4e6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+//     },
+//     {
+//       name: 'Pumpernickel',
+//       hasGluten: true,
+//       image: 'https://images.unsplash.com/photo-1586444248902-2f64eddc13df?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+//     }
+//   ]
   
