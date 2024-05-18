031d2e89de97e39d6cdd02b84d9ee43a1f05680c
“Foxshane997” <Foxshane997@gmail.com>
2024-05-18 10:03:36 -0700

    Added data

--- models/baker.js
+++ models/baker.js
@@ -4,9 +4,19 @@ const { Schema } = mongoose
 
 // Schema
 const bakerSchema = new Schema ({
-
+    name: {
+        type: String.apply,
+        required: true,
+        enum: ['Rachel', 'Monica', 'Joey', 'Chandler', 'Ross', 'Phoebe']
+    },
+    startDate: {
+        type: Date,
+        required: true
+    },
+    bio: String
 })
 
 // Model & Export
 const Baker = mongoose.model('baker', bakerSchema)
 module.exports = Baker
+ 
