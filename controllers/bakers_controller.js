06e31f83cadfdc64ad1d8e09f9f8928be55043d1
“Foxshane997” <Foxshane997@gmail.com>
2024-05-18 10:29:06 -0700

    Fixed the issue. Redirected to correct page

--- controllers/bakers_controller.js
+++ controllers/bakers_controller.js
@@ -4,5 +4,10 @@ const baker = express.Router()
 const Baker = require('../models/baker.js')
 const bakerSeedData = require('../models/baker_seed.js')
 
+baker.get('/data/seed', (req, res) => {
+    Baker.insertMany(bakerSeedData)
+        .then(res.redirect('/breads'))
+})
+
 // Export
 module.exports = baker                    
